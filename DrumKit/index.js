6171135b04316414ff0b22365c7feef9ea96dd21
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-02-09 15:37:56 -0500

    Added bg images, keyboard listener
    Refactored audio code to use common logic

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,9 +1,51 @@
-
 var drums = document.querySelectorAll(".drum");
 var numDrumBtns = drums.length;
 
-for(var i = 0; i < numDrumBtns; i++){
-    drums[i].addEventListener("click", function () {
-        alert("I got clicked");
-    });
+for (var i = 0; i < numDrumBtns; i++) {
+  drums[i].addEventListener("click", function () {
+    var buttonInnerHtml = this.innerHTML;
+    playAudio(buttonInnerHtml);
+
+  });
+}
+
+document.addEventListener("keydown", function(event) {
+    playAudio(event.key);
+});
+
+function playAudio(key){
+    switch (key) {
+        case "w":
+          var audio = new Audio("sounds/tom-1.mp3");
+          audio.play();
+          break;
+        case "a":
+          var audio = new Audio("sounds/tom-2.mp3");
+          audio.play();
+          break;
+        case "s":
+          var audio = new Audio("sounds/tom-3.mp3");
+          audio.play();
+          break;
+        case "d":
+          var audio = new Audio("sounds/tom-4.mp3");
+          audio.play();
+          break;
+        case "j":
+          var audio = new Audio("sounds/snare.mp3");
+          audio.play();
+          break;
+        case "k":
+          var audio = new Audio("sounds/crash.mp3");
+          audio.play();
+          break;
+        case "l":
+          var audio = new Audio("sounds/kick-bass.mp3");
+          audio.play();
+          break;
+  
+        default:
+          console.log(key);
+          break;
+      }
 }
