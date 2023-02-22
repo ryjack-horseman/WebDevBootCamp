aae7c3732359949343187a1ffd7c65caa9226aba
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-02-22 16:17:31 -0500

    Set up Newsletter project and added signup frontend

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
+  });
+  
