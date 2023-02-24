baf428cc488f58ec99475a6ace0915ebe87fe9c3
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-02-24 18:24:16 -0500

    Update to add new list items

--- Todolist-v1/app.js
+++ Todolist-v1/app.js
@@ -3,15 +3,29 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var listItems = [];
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
     var today = new Date();
     //get day as "Monday, Tuesday, etc..."
-    const options = { weekday: "long" };
+    const options = {
+         weekday: "long",
+         day: "numeric",
+         month: "long"
+    };
+
     var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", { kindOfDay: day });
+    res.render("list", {kindOfDay: day, newListItems: listItems});
+});
+
+app.post("/", function(req, res){
+    var listItem = String(req.body.newItem);
+    listItems.push(listItem);
+    res.redirect("/");
 });
 
 app.listen(3000, function () {
