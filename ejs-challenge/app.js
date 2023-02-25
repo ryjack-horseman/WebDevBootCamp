e33006c6ac9fbf70af33a4a14b50dd8630845d83
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-02-25 12:58:20 -0500

    Finished Challenge 1
    Added H1 to home page using ejs and Node

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -15,19 +15,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res){
+    res.render("home");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, function(){
+  console.log("Server Started at localhost:3000");
 });
