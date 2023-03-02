205cd1aebaf66a6f942e275ae5ab2f7f8024cfa7
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-03-02 17:20:03 -0500

    Attach DB to TodoList
    Added DB to webpage

--- Todolist-v2/app.js
+++ Todolist-v2/app.js
@@ -6,66 +6,81 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/todolistDB");
 
 const itemsSchema = {
-  name: String
+  name: String,
 };
 
 const Item = mongoose.model("Item", itemsSchema);
 
 const item1 = new Item({
-  name: "Welcome to your TodoList!"
+  name: "Welcome to your TodoList!",
 });
 
 const item2 = new Item({
-  name: "Hit the + button to add a new item."
+  name: "Hit the + button to add a new item.",
 });
 
 const item3 = new Item({
-  name: "<-- Hi this  to delete an item."
+  name: "<-- Hi this  to delete an item.",
 });
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems)
-.then(function(items) {
-    console.log("Success saved default items to db");
-  })
-  .catch(function(err) {
-    console.log(err);
+app.get("/", function (req, res) {
+  getItems().then((list) => {
+    if (list.length === 0) {
+      Item.insertMany(defaultItems)
+        .then(function (items) {
+          console.log("Success saved default items to db");
+          res.redirect("/");
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    }else{
+      res.render("list", { listTitle: "Today", newListItems: list });
+    }
   });
-
-app.get("/", function(req, res) {
-  res.render("list", {listTitle: "Today", newListItems: defaultItems});
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName
+  });
+  item.save();
+  res.redirect("/");
+});
 
-  const item = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  console.log(checkedItemId);
+  Item.findByIdAndRemove(checkedItemId).then(x => {
     res.redirect("/");
-  }
+  });
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
+
+async function getItems() {
+  //const items = await Item.find();
+  return await Item.find();
+}
