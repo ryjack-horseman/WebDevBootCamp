433554aaa321dea19b0d06386796a7e1e6049e38
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-03-14 16:23:01 -0400

    Add environment vars

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -20,8 +21,8 @@ const userSchema = new mongoose.Schema({
     password: String,
 });
 
-const secret = "THISISOURLITTLESECRET.";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = mongoose.model("User", userSchema);
 
