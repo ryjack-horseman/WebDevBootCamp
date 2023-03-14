9c14897ceeb23f0ab519162dc2d8e17582bafd6e
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-03-14 16:10:10 -0400

    Level 2, encryption added
    encryption key is visible

--- Secrets/app.js
+++ Secrets/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -14,10 +15,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-};
+});
+
+const secret = "THISISOURLITTLESECRET.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 const User = mongoose.model("User", userSchema);
 
