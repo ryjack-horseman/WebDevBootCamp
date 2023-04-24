675bb9127cd83c076cf7deb5914c098f9c87e5d4
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-04-24 09:22:09 -0400

    React Dependencies start code update

--- KeeperApp/keeper-app/src/components/CreateArea.jsx
+++ KeeperApp/keeper-app/src/components/CreateArea.jsx
@@ -32,7 +32,7 @@ function CreateArea(props) {
 
   return (
     <div>
-      <form>
+      <form className="create-note">
         <input onChange={handleChange} name="title" placeholder="Title" value={note.title} />
         <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={note.content}/>
         <button onClick={submitNote}>Add</button>
