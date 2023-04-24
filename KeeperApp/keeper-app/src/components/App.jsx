45ccd3f59674a64721b7675a0f0c53d05fe1c79a
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-04-24 09:18:56 -0400

    Finished Keeper part 3 project
    connected various React components

--- KeeperApp/keeper-app/src/components/App.jsx
+++ KeeperApp/keeper-app/src/components/App.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+    const [items, setItems] = useState([]);
+
+    function addNote(note){
+        setItems(prevNotes => {
+            return [...prevNotes, note];
+        });
+    }
+
+    function deleteNote(id){
+        setItems(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
     return (
         <div>
           <Header />
-          <CreateArea />
-          <Note key={1} title="Note title" content="Note content" />
+          <CreateArea
+          onAdd={addNote} />
+
+          <div>
+            <ul>
+                {items.map((noteItem, index) => (
+                    <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote}/>
+                ))}
+            </ul>
+          </div>
           <Footer />
         </div>
       );
